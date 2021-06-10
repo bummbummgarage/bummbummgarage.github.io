49eef6012e03e42e7120cbb10a56d55a1ee1e752
Steffen <steffenlohaus@googlemail.com>
2021-06-10 18:46:00 +0200

    Update Bumm-Bumm-Garage-Sequencer-II-0.1-Sketch.ino

--- modules/sequencer-ii-0.1/Bumm-Bumm-Garage-Sequencer-II-0.1-Sketch/Bumm-Bumm-Garage-Sequencer-II-0.1-Sketch.ino
+++ modules/sequencer-ii-0.1/Bumm-Bumm-Garage-Sequencer-II-0.1-Sketch/Bumm-Bumm-Garage-Sequencer-II-0.1-Sketch.ino
@@ -4,7 +4,7 @@
 */
 
 // General
-const bool debug = false; // Enables the Serial print in several functions. Slows down the frontend.
+const bool debug = true; // Enables the Serial print in several functions. Slows down the frontend.
 const int pushButtonDelay = 50; // The time a button will be muted after last change.
 const int longPress = 2000; // The time to trigger secondary actions on buttons.
 
