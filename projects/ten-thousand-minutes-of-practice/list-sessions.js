74a96d58ff6e62b4b6561accc847490d5c550658
Steffen <steffenlohaus@googlemail.com>
2021-11-15 17:47:51 +0100

    Better URL breaks (mobile view).

--- projects/ten-thousand-minutes-of-practice/list-sessions.js
+++ projects/ten-thousand-minutes-of-practice/list-sessions.js
@@ -5,9 +5,9 @@ $( document ).ready(function() {
 	// Add sessins sections
 	$("#content").append( "<div id='sessions'></div>" );
 	// Add heading to the section
-	$("#content").append("<h2>Sessions</h2>");
+	$("#sessions").append("<h2>Sessions</h2>");
 	// Setup table
-	$("#content").append("<table><thead><tr><th>Date</th><th>Minutes</th><th>Link</th></tr></thead><tbody></tbody></table>");
+	$("#sessions").append("<table><thead><tr><th>Date</th><th>Minutes</th><th>Link</th></tr></thead><tbody></tbody></table>");
 	// Add entries to the table
 	$.get( "https://opensheet.vercel.app/1uzUp3vcmD3A8ZHVXM3ih6do9SHKS0ISE8IcS67cavKY/RawData", function( data ) {
 
@@ -31,7 +31,7 @@ $( document ).ready(function() {
 			row += "<td>";
 			var link = $( this )[0].Link;
 			var text = link.replace("https://","");
-			row += "<a href='"+link+"'>"+text+"</a>";
+			row += "<a style='word-break: break-all;' href='"+link+"'>"+text+"</a>";
 			row += "</td>";
 
 			row += "</tr>";
